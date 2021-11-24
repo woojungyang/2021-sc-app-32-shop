b9e01f549b1fc626b285946100fc3f622f4a6fc9
woojungyang <blossom1113@nate.com>
2021-11-24 13:59:10 +0900

    img 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './app/store';
+// import { store } from './app/store';
+
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,9 +11,9 @@ import './assets/css/base.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
